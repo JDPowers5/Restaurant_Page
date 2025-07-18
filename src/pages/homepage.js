6bb3ce38c22a99f19450ca3a3c8cb3bd7ae9414f
JDPowers5 <jeffrey@ee-jay.com>
2025-07-18 16:46:31 -0500

    fixed spacing error

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -12,10 +12,10 @@ function homepage() {
     image.alt = "young girl dressed as chef";
 
     const header = document.createElement("h1")
-    header.textContent="Welcome to Quinn's Diner"
+    header.textContent = "Welcome to Quinn's Diner"
 
     const span = document.createElement("span")
-    span.textContent= `Quinn's Diner was inspired by our daugther Quinn! At a young age Quinn enjoyed 
+    span.textContent = `Quinn's Diner was inspired by our daugther Quinn! At a young age Quinn enjoyed 
                      picking our fresh raspberries from our garden so much. She inspired us to grow 
                      all of our own food. Since then we have expanded our garden and we would like to 
                      share with you our favorite recipes with every item sourced here right outside 
